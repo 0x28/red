6d023700275048592455d0ed6f62ae1634a58a01
0x28 <12886563+0x28@users.noreply.github.com>
2021-05-17 22:51:51 +0200

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,11 @@ impl TerminalReset {
 impl Drop for TerminalReset {
     fn drop(&mut self) {
         // NOTE: Don't panic while dropping!
-        let _ignore =
-            termios::tcsetattr(STDIN_FILENO, TCSAFLUSH, &self.original);
+        if let Err(e) =
+            termios::tcsetattr(STDIN_FILENO, TCSAFLUSH, &self.original)
+        {
+            eprintln!("tcsetattr error: {}", e)
+        }
     }
 }
 
@@ -29,19 +32,19 @@ const STDIN_FILENO: i32 = 0;
 // const STDERR_FILENO: i32 = 2;
 
 fn enable_raw_mode() {
-    let mut attr = Termios::from_fd(STDIN_FILENO).unwrap();
+    let mut attr = Termios::from_fd(STDIN_FILENO).expect("tcgetattr");
     attr.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     attr.c_oflag &= !(OPOST);
     attr.c_cflag |= CS8;
     attr.c_lflag &= !(ECHO | ICANON | IEXTEN | ISIG);
     attr.c_cc[VMIN] = 0;
     attr.c_cc[VTIME] = 1;
-    termios::tcsetattr(STDIN_FILENO, TCSAFLUSH, &attr).unwrap();
+    termios::tcsetattr(STDIN_FILENO, TCSAFLUSH, &attr).expect("tcsetattr");
 }
 
 fn main() {
     let _term_reset =
-        TerminalReset::new(Termios::from_fd(STDIN_FILENO).unwrap());
+        TerminalReset::new(Termios::from_fd(STDIN_FILENO).expect("tcgetattr"));
     enable_raw_mode();
 
     loop {
